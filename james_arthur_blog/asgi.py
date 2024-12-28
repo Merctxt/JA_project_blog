4f39f9c9d66859ea6757c197d9873184b7628f7d
Merctxt <giovannihettwer2@gmail.com>
2024-12-27 22:04:30 -0300

    added new project files

---
+++ james_arthur_blog/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for james_arthur_blog project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'james_arthur_blog.settings')
+
+application = get_asgi_application()
