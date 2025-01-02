40424df0b042446a83c05c225eb96a83b0fd6b91
Merctxt <giovannihettwer2@gmail.com>
2025-01-01 21:56:24 -0300

    html and css updated

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from .models import Page
+from django.contrib import messages
 
 # Views (definições iniciais renderizando templates)
 def index(request):
